8e54ee677a8a93dc8b3b51816f2cc8c2575c138b
Ihosvany Camps <icamps@gmail.com>
2026-01-30 19:13:16 -0300

    Update BiEntropy_best.sh

--- BiEntropy_best.sh
+++ BiEntropy_best.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
+#
+# 2026-01-30
+#
+# Run as: sh BiEntropy_best.sh root_system_name number_files_to_process
+#         sh BiEntropy_best.sh MBNNB 30
+#
 # Script to get the files of molecule with most probable BiEntropy
+# What the script does:
 # 1. Get inside each system folder
 # 2. Get inside each concentration folder
 # 3. Calculate the entropy of each structure. Create a file named BiEbtropy_system.dat. Move the file to out_BiEntropy folder
@@ -20,14 +27,17 @@ mkdir out_BiEntropy
 rm -fr out_Best_Structures 2> /dev/null
 mkdir out_Best_Structures
 
-ln -s ~/Quantum_Chemistry/scripts/BiEntropy.sh .  2> /dev/null
-for d1 in g-C3N4*; do      # System folders
+
+filename_root=$1
+number_files=$2
+
+for d1 in $filename_root*; do      # System folders
     cd ${d1}
 #    echo ${d}
     for d2 in *; do
         cd ${d2}
         echo ${d1} ${d2}
-        "${PYENV_ROOT}/versions/$Pv2/bin/python" ../../BiEntropy.sh ${d1} ${d2} 1000
+        "${PYENV_ROOT}/versions/$Pv2/bin/python" ../../BiEntropy.sh ${d1} ${d2} $number_files
         mv BiEntropy*.dat ../../out_BiEntropy/
         cd ..
     done
@@ -36,38 +46,21 @@ done
 #pwd
 
 cd out_BiEntropy/
-ln -s ~/Quantum_Chemistry/scripts/BiEntropy_getIndex.py . 2> /dev/null
 
 for file in *.dat; do
    extension="${file##*.}"
    filename="${file%.*}"
    prefix="BiEntropy_"
    foo="${filename#"$prefix"}"
-   echo $foo-$("${PYENV_ROOT}/versions/$Pv3/bin/python" BiEntropy_getIndex.py $filename).xyz >> Best_structures.csv
+   echo $foo-$("${PYENV_ROOT}/versions/$Pv3/bin/python" ../BiEntropy_getIndex.py $filename).xyz >> Best_structures.csv
 done
 cd ..
 
-for d1 in g-C3N4*; do      # System folders
+for d1 in $filename_root*; do      # System folders
    cd ${d1}
    while read line; do
    filename=$(echo $line | awk '{printf "%s \n",$1}')
    find -maxdepth 12 -iname ${filename} -exec cp {} ../out_Best_Structures/ \;
-   done < out_BiEntropy/Best_structures.csv
+   done < ../out_BiEntropy/Best_structures.csv
    cd ..
 done
-
-
-while read line; do
-         filename=$(echo $line | awk '{printf "%s \n",$1}')
-         echo ${filename}
-         find g-C3N4*/ -maxdepth 4 -name ${filename} -exec cp {} out_Best_Structures/ \;
-done < out_BiEntropy/Best_structures.csv
-
-
-exit
-
-
-
-
-
-
